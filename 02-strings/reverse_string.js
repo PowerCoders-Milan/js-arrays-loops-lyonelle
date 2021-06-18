2f1f6dd97d7d9d526aa14c126be45c7a74edfd6c
lyonelle <ambouloulyo@gmail.com>
2021-06-18 15:08:57 +0200

    exercise # done

--- 02-strings/reverse_string.js
+++ 02-strings/reverse_string.js
@@ -1,1 +1,12 @@
 // read carefully the instructions
+ 
+let myString = 'prova vedrai se funziona';
+let stringArray = myString.split('');
+console.log(stringArray);
+
+let  reverse = stringArray.reverse();
+console.log(reverse);
+ 
+console.log(stringArray.join(''))
+
+
