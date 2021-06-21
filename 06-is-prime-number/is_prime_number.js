14e22cbb63605a76be315b33c22633c914dec4c6
lyonelle <ambouloulyo@gmail.com>
2021-06-21 16:56:18 +0200

    esercizio # completato

--- 06-is-prime-number/is_prime_number.js
+++ 06-is-prime-number/is_prime_number.js
@@ -1,8 +1,25 @@
 // check if a number is prime or not
 const isPrime = number => {
-
+for(let i = number - 1; i> 1; i--){
+    if (number % i === 0) {
+        return false;
+    } 
+        
+    }
+    return true
 }
+
 
 const getPrimes = amount => {
-    
+    const primeNumbers = [];
+  let i = 1 ;
+  while (primeNumbers.length <= amount ) {
+if(isPrime(i)) {
+    primeNumbers.push(i);
 }
+i++;
+  }
+  return primeNumbers
+}
+//console.log(getPrimes(50).length);
+console.log(getPrimes(7000))
