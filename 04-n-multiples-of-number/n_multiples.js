9b73f46ad17dda88e6b83dab397e7c4379e72602
lyonelle <ambouloulyo@gmail.com>
2021-06-21 15:48:46 +0200

    esercizio # completato

--- 04-n-multiples-of-number/n_multiples.js
+++ 04-n-multiples-of-number/n_multiples.js
@@ -1,6 +1,20 @@
 // function declaration
 const nMultiplesOf = (number, limit) => {
+    const multiples = [];
+   let  i = 0 ;
+   
+    while (multiples.length <= limit ){ 
+        if(i%number === 0) {
+          multiples.push(i); 
+           
 
+       }
+        
+     
+        i++;
+    }
+    
+  return multiples
 }
 
 console.log(nMultiplesOf(2, 10))
