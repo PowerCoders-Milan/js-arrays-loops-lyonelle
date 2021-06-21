291a08e79d19dc599ca117cb6555ebf83bbef290
lyonelle <ambouloulyo@gmail.com>
2021-06-21 10:13:43 +0200

    exercise # done

--- 03-multiples-of-number/multiples_of_number.js
+++ 03-multiples-of-number/multiples_of_number.js
@@ -1,1 +1,18 @@
 // define your function here
+
+//const multiplesOfNumber = multiplesOf(number)
+const multiplesOf = (number, limit) => {
+    const multiples = [];
+     for ( i = 0; i <limit; i++){
+      if (i%number === 0) { 
+
+        multiples.push(i);
+     } 
+       
+     
+    }    
+      return multiples;     
+        }
+ 
+ const multiplesOfTwo = multiplesOf(4, 18)   ;    
+console.log(multiplesOfTwo );
