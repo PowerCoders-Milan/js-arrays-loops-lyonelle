75d9f7a6c3662bc15130d6c07aa6a44c7867dbb6
lyonelle <ambouloulyo@gmail.com>
2021-06-21 16:29:00 +0200

    esercizio # completato

--- 05-fibonacci/fibonacci.js
+++ 05-fibonacci/fibonacci.js
@@ -1,4 +1,27 @@
 // program to generate fibonacci series up to a certain number
 const maxNumber = Math.ceil(Math.random()*200);
-let n1 = 0, n2 = 1, nextTerm;
+
+const getFibonacciSequence = maxNumber => {
+    let  n1 = 0; n2 = 1; nextTerm = null;
+        const fibonacciSequence = [n1, n2];
+        nextTerm= n1 + n2
+       
+        // console.log("fibonacci di sequenza"); 
+        // console.log(n1);
+        // console.log(n2);
+
+    while (nextTerm <= maxNumber) {
+        fibonacciSequence.push(nextTerm);
+        
+		 console.log(nextTerm)
+		n1 = n2
+		n2 = nextTerm
+		nextTerm = n1 + n2
+	}
+return fibonacciSequence;
+}
+getFibonacciSequence(maxNumber);
+
+
+
 
